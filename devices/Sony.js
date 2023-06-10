98b940cf4cb3d149c4efc0943e5f85353d7fb499
maniacx <macs18max@gmail.com>
2023-06-09 19:12:58 -0500

    Battery Health Charging: sony: add express charging if supported

--- devices/Sony.js
+++ devices/Sony.js
@@ -7,6 +7,7 @@ const Helper = Me.imports.lib.helper;
 const {fileExists, readFileInt, runCommandCtl} = Helper;
 
 const SONY_PATH = '/sys/devices/platform/sony-laptop/battery_care_limiter';
+const SONY_HIGHSPEED_CHARGING_PATH = '/sys/devices/platform/sony-laptop/battery_highspeed_charging';
 
 var SonySingleBattery = GObject.registerClass({
     Signals: {'threshold-applied': {param_types: [GObject.TYPE_BOOLEAN]}},
@@ -28,29 +29,56 @@ var SonySingleBattery = GObject.registerClass({
     isAvailable() {
         if (!fileExists(SONY_PATH))
             return false;
+        if (fileExists(SONY_HIGHSPEED_CHARGING_PATH))
+            this.deviceHaveExpressMode = true;
         return true;
     }
 
     async setThresholdLimit(chargingMode) {
-        let batteryCareLimiter;
-        if (chargingMode === 'ful')
+        let batteryCareLimiter, highspeedCharging, status;
+        if (chargingMode === 'ful') {
             batteryCareLimiter = 0;
-        else if (chargingMode === 'bal')
+            highspeedCharging = 0;
+        } else if (chargingMode === 'bal') {
             batteryCareLimiter = 80;
-        else if (chargingMode === 'max')
+            highspeedCharging = 0;
+        } else if (chargingMode === 'max') {
             batteryCareLimiter = 50;
-        if (readFileInt(SONY_PATH) === batteryCareLimiter) {
-            this.endLimitValue = batteryCareLimiter === 0 ? 100 : batteryCareLimiter;
-            this.emit('threshold-applied', true);
-            return 0;
+            highspeedCharging = 0;
+        } else if (chargingMode === 'exp') {
+            batteryCareLimiter = 0;
+            highspeedCharging = 1;
         }
-        const status = await runCommandCtl('SONY', `${batteryCareLimiter}`, null, false);
-        if (status === 0) {
-            const endLimitValue = readFileInt(SONY_PATH);
-            if (batteryCareLimiter === endLimitValue) {
-                this.endLimitValue = endLimitValue === 0 ? 100 : endLimitValue;
+
+        if (highspeedCharging === 1) {
+            status = await runCommandCtl('SONY', `${batteryCareLimiter}`, null, false);
+            if (status === 0 && (readFileInt(SONY_PATH) === batteryCareLimiter))
+                status = await runCommandCtl('SONY_HIGHSPEED_CHARGING', `${highspeedCharging}`, null, false);
+            if (status === 0 && readFileInt(SONY_HIGHSPEED_CHARGING_PATH) === highspeedCharging) {
+                this.endLimitValue = 100;
+                this.mode = chargingMode;
                 this.emit('threshold-applied', true);
                 return 0;
+            }
+        } else {
+            status = 0;
+            if (this.deviceHaveExpressMode) {
+                status = await runCommandCtl('SONY_HIGHSPEED_CHARGING', `${highspeedCharging}`, null, false);
+                if (status === 0 && readFileInt(SONY_HIGHSPEED_CHARGING_PATH) === highspeedCharging)
+                    this.mode = chargingMode;
+                else
+                    status = 1;
+            }
+            if (status === 0) {
+                status = await runCommandCtl('SONY', `${batteryCareLimiter}`, null, false);
+                if (status === 0) {
+                    const endLimitValue = readFileInt(SONY_PATH);
+                    if (batteryCareLimiter === endLimitValue) {
+                        this.endLimitValue = endLimitValue === 0 ? 100 : endLimitValue;
+                        this.emit('threshold-applied', true);
+                        return 0;
+                    }
+                }
             }
         }
         this.emit('threshold-applied', false);
